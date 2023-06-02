8f88a460c3f29a80fa9fce5fcfca6d6e42627048
Ibnballo1 <belloabdullateef035@gmail.com>
2023-06-02 11:54:12 +0100

    Decode a char and word

---
+++ decode.rb
@@ -0,0 +1,34 @@
+@morse_character = {
+  A: '.-', B: '-...', C: '-.-.', D: '-..', E: '.', F: '..-.', G: '--.',
+  H: '....', I: '..', J: '.---', K: '-.-', L: '.-..', M: '--', N: '-.',
+  O: '---', P: '.--.', Q: '--.-', R: '.-.', S: '...', T: '-', U: '..-',
+  V: '...-', W: '.--', X: '-..-', Y: '-.--', Z: '--..', '1' => '.----',
+  '2' => '..---', '3' => '...--', '4' => ' ....-', '5' => '.....',
+  '6' => '-....', '7' => '--...', '8' => '---..', '9' => '----.',
+  '10' => '-----'
+}
+
+# Create a method to decode a Morse code character, takes a string parameter,
+# and return the corresponding character in uppercase (e.g. decode_char(".-") returns "A").
+
+def decode_char(char)
+  # get the value of the morse_character
+  @morse_character.each do |key, value|
+    print key.to_s if value == char
+  end
+end
+
+# Create a method to decode an entire word in Morse code, takes a string parameter,
+# and return the string representation. Every character in a word will be separated by a
+# single space (e.g. decode_word("-- -.--") returns "MY").
+
+def decode_word(word)
+  new_word = ''
+  # loop through the word
+  word.split(' ').each do |char|
+    new_word += decode_char(char).to_s
+  end
+end
+
+# decode_char('.-')
+decode_word('-- -.--')
